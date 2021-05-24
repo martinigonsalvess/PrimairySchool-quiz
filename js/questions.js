5dd8c33b7fba5eb1cc7244d73b24eb3d9214aef6
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-05-24 15:07:30 +0200

    quiz questions

---
+++ js/questions.js
@@ -0,0 +1,16 @@
+//creating an array and passing the number, questions, options and answers
+
+let questions = [
+  {
+    numb: 1,
+    question: "Uit welk land komt pizza?",
+    answer: "Italië",
+    options: ["Nederland", "Spanje", "Italië", "Duitsland"],
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "",
+    options: [],
+  },
+];
--- js/script.js
+++ js/script.js
@@ -4,8 +4,20 @@ const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
 const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
 
 // if start quiz button clicked
 start_btn.onclick = () => {
   info_box.classList.add("activeInfo");
+};
+
+//if exit button click
+exit_btn.onclick = () => {
+  info_box.classList.remove("activeInfo");
+};
+
+//if continue button click
+continue_btn.onclick = () => {
+  info_box.classList.remove("activeInfo");
+  quiz_box.classList.add("activeQuiz");
 };
